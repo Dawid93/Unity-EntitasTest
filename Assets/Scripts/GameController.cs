b11d490b93fe400087efa45813be86440748c305
Dave <ddawid@windowslive.com>
2020-11-15 17:05:36 +0100

    Import Entitas and create test code generate

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Hello World");        
+    }
+}
